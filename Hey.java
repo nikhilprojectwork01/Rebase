ecce2c134b9ec59e5cf48853375320820c809a45
Nikhil Pandey <167502120+nikhilprojectwork01@users.noreply.github.com>
2025-05-29 13:25:16 +0530

    Update Hey.java

--- Hey.java
+++ Hey.java
@@ -3,5 +3,7 @@ package Hello;
 public class Hey {
   public static void main(String[] args) {
     System.out.println("Hello Nice to meet you");
+    int num = 30 ;
+    String name = "Nikhil Pandey"
   }
 }
